e3244e94873ef1a89f09c1b7b7aaee31ecd791e3
vathuglife <thuandt582@gmail.com>
2025-05-18 17:17:40 +0700

    dva: Applies default Keyword value to GetBlogsRequest

--- OdysseyPortfolio-Libraries/Payloads/Request/GetBlogsRequest.cs
+++ OdysseyPortfolio-Libraries/Payloads/Request/GetBlogsRequest.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace OdysseyPortfolio_Libraries.Payloads.Request
 {
     public class GetBlogsRequest
-    {
-        public string Keyword { get; set; } = "";
+    {        
+        public string? Keyword { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }        
     }
